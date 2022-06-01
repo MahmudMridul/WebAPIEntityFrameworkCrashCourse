019ac9e8557b2545833d1a8b26c45d17ca539f20
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-01 21:48:15 +0600

    Update Delete Post Apis

--- DotnetWebApi/Controllers/ContactsController.cs
+++ DotnetWebApi/Controllers/ContactsController.cs
@@ -36,5 +36,71 @@ namespace DotnetWebApi.Controllers
             return FoundContact;
         }
 
+        [HttpPost]
+        public async Task<ActionResult<Contact>> AddContact(Contact NewContact)
+        {
+            _context.Contacts.Add(NewContact);
+            await _context.SaveChangesAsync();
+
+            return NewContact;
+        }
+
+        [HttpPut("placeupdate")]
+        public async Task<ActionResult<Contact>> UpdatePlace(int id, string NewPlace)
+        {
+            Contact OldContact = await _context.Contacts.FindAsync(id);
+
+            if (OldContact == null) { return NotFound("Not Found"); }
+            if (OldContact.IsDeleted == true) { return NotFound("Contact was deleted"); }
+
+            OldContact.Place = NewPlace;
+            await _context.SaveChangesAsync();
+
+            return OldContact;
+        }
+
+        [HttpPut("recover")]
+        public async Task<ActionResult<Contact>> RecoverContact(int id)
+        {
+            Contact FoundContact = await _context.Contacts.FindAsync(id);
+
+            if (FoundContact == null) { return NotFound("Not Found"); }
+            if (FoundContact.IsDeleted == true) 
+            {
+                FoundContact.IsDeleted = false;
+                await _context.SaveChangesAsync();
+            }
+
+            return FoundContact;
+        }
+
+        [HttpDelete("deleteforever")]
+        public async Task<ActionResult<Contact>> DeleteContactPerm(int id)
+        {
+            Contact FoundContact = await _context.Contacts.FindAsync(id);
+
+            if (FoundContact == null) { return NotFound("Not Found"); }
+            if (FoundContact.IsDeleted == true) { return NotFound("Contact was deleted"); }
+
+            _context.Contacts.Remove(FoundContact);
+            await _context.SaveChangesAsync();
+
+            return FoundContact;
+        }
+
+        [HttpDelete("delete")]
+        public async Task<ActionResult<Contact>> DeleteContactTemp(int id)
+        {
+            Contact FoundContact = await _context.Contacts.FindAsync(id);
+
+            if (FoundContact == null) { return NotFound("Not Found"); }
+            if (FoundContact.IsDeleted == true) { return NotFound("Contact was deleted"); }
+
+            FoundContact.IsDeleted = true;
+            await _context.SaveChangesAsync();
+
+            return FoundContact;
+        }
+
     }
 }
