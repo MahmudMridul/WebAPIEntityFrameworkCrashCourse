4a343d145f897f6efbe56ff1c6edc8c0ed5afbac
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-01 18:36:47 +0600

    Async Apis

---
+++ DotnetWebApi/Controllers/ContactsController.cs
@@ -0,0 +1,40 @@
+﻿using DotnetWebApi.Data;
+using DotnetWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ContactsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _context.Contacts.Where(c => c.IsDeleted == false).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(int id)
+        {
+            Contact FoundContact = await _context.Contacts.FindAsync(id);
+
+            if (FoundContact == null) { return NotFound("Not Found"); }
+            if (FoundContact.IsDeleted == true) { return NotFound("Contact was deleted"); }
+
+            return FoundContact;
+        }
+
+    }
+}
