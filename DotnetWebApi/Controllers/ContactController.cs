a45648439db5e1a5aa0fad22d463660c6ca1de46
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-01 11:42:02 +0600

    Contact controller added

---
+++ DotnetWebApi/Controllers/ContactController.cs
@@ -0,0 +1,55 @@
+﻿using DotnetWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotnetWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+
+        private List<Contact> Contacts = new List<Contact>
+        {
+            new Contact { Id = 1, FirstName = "Peter", LastName = "Parker", NickName = "SpiderMan", Place = "NY" },
+            new Contact { Id = 2, FirstName = "Tony", LastName = "Stark", NickName = "IronMan", Place = "NY" },
+        };
+
+        // GET: api/<ContactController>
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return Contacts;
+        }
+
+        // GET api/<ContactController>/5
+        [HttpGet("{id}")]
+        public Contact Get(int id)
+        {
+            return Contacts.SingleOrDefault(contact => contact.Id == id);
+        }
+
+        // POST api/<ContactController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ContactController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ContactController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
