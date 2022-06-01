4cd32e63de55dc7ef7bb5e064f9ab333f335621a
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-01 11:09:40 +0600

    Contact class added

---
+++ DotnetWebApi/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DotnetWebApi.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Place { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
