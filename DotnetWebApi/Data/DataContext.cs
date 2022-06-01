c96c0ede6d7d1513a070cd12f0d8e2b06d2079d0
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-06-01 18:37:40 +0600

    Data context and migrations

---
+++ DotnetWebApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DotnetWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetWebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
